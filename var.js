fb93324d770929f57559452baa16dbfae5ed81f5
Ana Lu <luczynaki@gmail.com>
2014-12-07 10:20:36 -0600

    structure written

---
+++ var.js
@@ -0,0 +1,78 @@
+var library = {
+	//images
+	'pim': new Image(),
+	'magic': new Image(),
+	'portal': new Image(),
+	'poof': new Image(),
+	'skull': new Image(),
+
+	//music
+	'ambient': 'huh',
+
+	//ratios
+	'scr': [],
+	'canvas': [],
+	'multiplier': 0,
+	//[x, y, frame_x, frame_y]
+	'pim_size': [],
+	'magic_size': [],
+	'portal_size': [],
+	'poof_size': [],
+	'skull_size': [],
+
+	//dataaa
+	'data': {
+		'poop': true,
+		'highscore': null,
+		'rounds': 0,
+		'music': false,
+		'player': ['reverse', 'clockwise', 'counter-clockwise']
+	}
+};
+
+var game = {
+	//time
+	'start': null,
+	'end': null,
+	'total': 0,
+
+	//status
+	'running': false,
+	'finished': true,
+
+	//each round has new info
+	'round': 0,
+	'pims': [],
+	'skulls': [],
+	'magic': [],
+		//[pims, skulls, magic, portals = 3]
+	'limit': [null, null, null, 3],
+	'player': null
+};
+
+var elements = {
+	'opening': document.getElementById('open-screen'),
+	'game': document.getElementById('game-screen'),
+	'ending': document.getElementById('end-screen'),
+	'help_button': document.getElementById('game-help'),
+	'info': document.getElementById('info-screen'),
+	
+	'player_action': document.getElementById('game-action'),
+	
+	'info_round': document.getElementById('info-round'),
+	'info_time': document.getElementById('info-time'),
+	'info_lose': document.getElementById('info-close'),
+	
+	'end_message': document.querySelector('.rando'),
+	'end_ghosts': document.getElementById('end-ghost-count'),
+	'end_time': document.getElementById('end-time'),
+	'end_score': document.getElementById('end-score'),
+	
+	'play': document.getElementById('open-play'),
+	'play_again': document.getElementById('end-play')
+	
+	'highscore_open': document.getElementById('open-hs'),
+	'highscore_info': document.getElementById('info-highscore'),
+	'highscore_end': document.getElementById('end-hs'),
+
+};
