f131d3c96adbc8265c09062d93895f8e84b5448a
Ana Lu <luczynaki@gmail.com>
2015-01-03 20:47:18 -0600

    new concat method in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['src/*.js'],
+				dest: 'src/stuckInPimbo.js'
+			}
+		}
 	});
+
+	//load tasks
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	//register tasks
+	grunt.registerTask('dev', ['concat']);
 }
